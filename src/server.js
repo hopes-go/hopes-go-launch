634c1030d4cf07c78201cb149de417c39c17b615
Hopes & Go Developer <hopee@hopes-go.dev>
2026-09-04 19:24:54 -0500

    Serve simple welcome page at root; add public/index.html

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -8,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Serve static welcome page
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.use('/api', routes);
 
